7734beacda20b2666e897cdb1ea4575bd037a2c3
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2024-12-13 10:57:03 +0900

    class by rust

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,36 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyclass]
+struct Counter {
+    count: i32,
+}
+
+#[pymethods]
+impl Counter {
+    #[new]
+    fn new() -> Self {
+        Counter { count: 0 }
+    }
+
+    fn increment(&mut self) -> PyResult<()> {
+        self.count += 1;
+        Ok(())
+    }
+
+    fn get_count(&self) -> PyResult<i32> {
+        Ok(self.count)
+    }
+
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("Counter({})", self.count))
+    }
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rust_reversi(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<Counter>()?;
     Ok(())
 }
