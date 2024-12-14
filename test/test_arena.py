6bf5e5bb3484848d07c87d92511b260cf195d732
neodymium6 <104201402+neodymium6@users.noreply.github.com>
2024-12-14 23:13:17 +0900

    add test for arena class

--- test/test_arena.py
+++ test/test_arena.py
@@ -1,1 +1,21 @@
-from rust_reversi import Board, Turn, Color, Arena  # type: ignore
+from rust_reversi import Arena  # type: ignore
+import sys
+import os
+
+N_GAMES = 1000
+
+
+def test_arena():
+    python = sys.executable
+
+    random_player = os.path.join(os.path.dirname(__file__), "random_player.py")
+
+    arena = Arena([python, random_player], [python, random_player])
+
+    arena.play_n(N_GAMES)
+
+    wins1, wins2, draws = arena.get_stats()
+    pieces1, pieces2 = arena.get_pieces()
+
+    assert wins1 + wins2 + draws == N_GAMES
+    assert pieces1 + pieces2 > 0
